646c3468bf2586eee123d95e85aa6e9052b65be5
Gilmar Silva <gilmarheavensilva@hotmail.com>
2019-05-22 11:34:37 -0300

    Add files via upload

---
+++ data/contar_palavras_frase.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (void)
+{
+char frase[50];
+int i, flag=0, total=0;
+
+printf("Introduza a frase\n");
+fgets(frase,51,stdin);
+
+for(i=0;frase[i]!='\0';i++) {
+	if(!flag && frase[i]!=' ') {
+		flag=1;
+		total++;
+	}
+	else if(flag && frase[i]==' ')
+		flag=0;
+}
+printf("O numero de palavras e: %d\n",total);
+
+return 0;
+}
