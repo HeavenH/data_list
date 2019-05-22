2c9cb9e1feb07d5c6f42e519620a8108f802772f
Gilmar Silva <gilmarheavensilva@hotmail.com>
2019-05-22 11:35:52 -0300

    Update contar_palavras_frase.cpp

--- data/contar_palavras_frase.cpp
+++ data/contar_palavras_frase.cpp
@@ -2,23 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main (void)
-{
+int main (void) {
 char frase[50];
-int i, flag=0, total=0;
+int i, flag = 0, cont = 0;
 
-printf("Introduza a frase\n");
-fgets(frase,51,stdin);
+printf("Informe a frase\n");
+fgets(frase, 51, stdin);
 
-for(i=0;frase[i]!='\0';i++) {
-	if(!flag && frase[i]!=' ') {
+for(i = 0; frase[i] != '\0'; i++) {
+	if(!flag && frase[i] != ' ') {
 		flag=1;
-		total++;
+		cont++;
 	}
-	else if(flag && frase[i]==' ')
+	else if(flag && frase[i] == ' ')
 		flag=0;
-}
-printf("O numero de palavras e: %d\n",total);
+	}
+printf("O numero de palavras e: %d\n", cont);
 
 return 0;
 }
